8aac4d94c4bb290455e764dbb5a81d5fc3c17299
jean <ranoajeanalie@gmail.com>
2025-04-04 22:40:14 +0800

    initial commit

--- javascript.js
+++ javascript.js
@@ -6,17 +6,22 @@ buttons.forEach(button => {
         const value = button.textContent;
 
         if (value === '=') {
-            
             try {
                 display.textContent = eval(display.textContent.replace('x', '*').replace('÷', '/'));
             } catch {
                 display.textContent = 'Error';
             }
         } else if (value === 'C') {
-            
             display.textContent = ''; 
-        } else {
+        } else if (value === '%') {
             
+            if (display.textContent) {
+                display.textContent = eval(display.textContent) / 100;
+            }
+        } else if (value === '←') {
+            
+            display.textContent = display.textContent.slice(0, -1);
+        } else {
             if (display.textContent === '') {
                 display.textContent = value; 
             } else {
